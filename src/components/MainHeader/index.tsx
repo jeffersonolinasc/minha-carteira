e15fa3916e75294fc3fd64396a91bf46b0ba344b
unknown <Jefferson@DESKTOP-DSVMNRA.alefperetz.local>
2022-04-19 17:44:40 -0300

    init

---
+++ src/components/MainHeader/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Container } from './styles';
+
+
+const MainHeader =  () => {
+    return (
+        <Container>
+            <h1>MainHeader</h1>
+        </Container>
+    )
+    
+}
+
+export default MainHeader;
---
+++ src/pages/Signin/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Signin: React.FC = () => {
+    return(
+        <h1>Dashboard</h1>
+    );
+}
+
+
+export default Signin;
