0782447b3552af6cee60b1bd3b55202cf2bf5e65
Jefferson Nascimento <jefferson.oliv@hotmail.com>
2022-04-19 18:52:40 -0300

    main header profile

--- src/components/MainHeader/index.tsx
+++ src/components/MainHeader/index.tsx
@@ -1,14 +1,29 @@
-import React from 'react';
-import { Container } from './styles';
+import React, { useMemo } from 'react';
+import { Container, Profile, Welcome, UserName } from './styles';
+import emojis from '../../utils/emoji';
 
 
-const MainHeader =  () => {
+const MainHeader = () => {
+
+
+    //GERANDO UM EMOJI ALEATORIO CADA VEZ QUE CARREGA A PAGINA
+    const emoji = useMemo(() => {
+        const indice = Math.floor(Math.random() * emojis.length);
+        return emojis[indice];
+    }, []);
+
+
     return (
         <Container>
-            <h1>MainHeader</h1>
+            <h1>Toogle</h1>
+
+            <Profile>
+                <Welcome>Olá,{emoji}</Welcome>
+                <UserName>Jefferson Nascimento</UserName>
+            </Profile>
         </Container>
     )
-    
+
 }
 
 export default MainHeader;
